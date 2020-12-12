49dc5f7f889023e49a9a585cdad3e398a5aac371
Pakhomov <pahomov.1v@yandex.ru>
2020-12-12 22:25:48 +0700

    change procfile

--- HiddenSanta/__init__.py
+++ HiddenSanta/__init__.py
@@ -45,4 +45,4 @@ def server_error(error):
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
